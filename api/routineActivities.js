c558e4ae82c8eba14e97a00d657d976ae6e1bb65
jtjemzz09 <jtjemzz09@LAPTOP-292I2KQV>
2023-08-21 00:12:00 -0700

    modified routine activities

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -49,32 +49,6 @@ router.patch('/:routineActivityId', requireUser, requiredNotSent({requiredParams
   }
 });
 
-// // PATCH /api/routine_activities/:routineActivityId
-// router.patch('/:routineActivityId', requireUser, requiredNotSent({requiredParams: ['count', 'duration'], atLeastOne: true}), async (req, res, next) => {
-//   try {
-//     const {count, duration} = req.body;
-//     const {routineActivityId} = req.params;
-//     const routineActivityToUpdate = await getRoutineActivityById(routineActivityId);
-//     if(!routineActivityToUpdate) {
-//       next({
-//         name: 'NotFound',
-//         message: `No routine_activity found by ID ${routineActivityId}`
-//       })
-//     } else {
-//       if(!await canEditRoutineActivity(req.params.routineActivityId, req.user.id)) {
-//         res.status(403);
-//         next({name: "Unauthorized", message: "You cannot edit this routine_activity!"});
-//       } else {
-//         const updatedRoutineActivity = await updateRoutineActivity({id: req.params.routineActivityId, count, duration})
-//         res.send(updatedRoutineActivity);
-//       }
-//     }
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-
 // DELETE /api/routine_activities/:routineActivityId
 router.delete('/:routineActivityId', requireUser, async (req, res, next) => {
   try {
@@ -90,19 +64,6 @@ router.delete('/:routineActivityId', requireUser, async (req, res, next) => {
 
 
 
-// // DELETE /api/routine_activities/:routineActivityId
-// router.delete('/:routineActivityId', requireUser, async (req, res, next) => {
-//   try {
-//     if(!await canEditRoutineActivity(req.params.routineActivityId, req.user.id)) {
-//       res.status(403);
-//       next({name: "Unauthorized", message: "You cannot edit this routine_activity!"});
-//     } else {
-//       const deletedRoutineActivity = await destroyRoutineActivity(req.params.routineActivityId)
-//       res.send({success: true, ...deletedRoutineActivity});
-//     }
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+
 
 module.exports = router;
